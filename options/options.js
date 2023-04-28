36eb204fd9419c740114d2e62824c6c4db2d8057
Crowdbotics <engineering@crowdbotics.com>
2023-04-28 09:45:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-darkness-de-79652",
+  url: "https://falling-darkness-de-79652.botics.co",
+  api: "https://falling-darkness-de-79652.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
